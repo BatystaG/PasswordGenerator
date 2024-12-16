3821aecf6e986052d70bef0b1b4202450c2c8252
Abner Garbin <abner.batysta151@gmail.com>
2024-12-16 14:22:44 -0300

    Primeiros passos, criando serviço

--- Application/Program.java
+++ Application/Program.java
@@ -1,4 +1,23 @@
 package Application;
 
+import Services.PasswordGenerator;
+
+import java.util.Scanner;
+
 public class Program {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o número de caracteres desejado para gerar a senha");
+        int lenght = sc.nextInt();
+
+        PasswordGenerator password = new PasswordGenerator();
+
+        password.passwordGenerator(lenght);
+
+
+        sc.close();
+    }
+
 }
--- Services/PasswordGenerator.java
+++ Services/PasswordGenerator.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+package Services;
+
+import java.security.SecureRandom;
+import java.util.Random;
+import java.util.Scanner;
 
 public class PasswordGenerator {
+
+    public PasswordGenerator(){
+    }
+
+    public String passwordGenerator(int lenght){
+
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        String result = "";
+
+        String normalChar = "abcdefghijklmnopqrstuvwxyz";
+        String upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String numChar = "0123456789";
+        String espChar = "!@#$%&*";
+
+        System.out.println("Deseja que a senha possua letras? (y/n)");
+        String letter = sc.nextLine();
+        System.out.println("Deseja que a senha possua letras maiúsculas? (y/n)");
+        String upper = sc.nextLine();
+        System.out.println("Deseja que a senha possua números? (y/n)");
+        String number = sc.nextLine();
+        System.out.println("Deseja que a senha possua caracteres especiais? (y/n)");
+        String esp = sc.nextLine();
+
+        if (letter.equals("y") && upper.equals("Y") && number.equals("y") && esp.equals("y")){
+            for (int i = 0; i < lenght; i++){
+
+            }
+        }
+
+
+
+
+
+        sc.close();
+
+        return result;
+    }
 }
